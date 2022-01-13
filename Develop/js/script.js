2dbc4afad1d569d21ab9f22bc101c62a45f419d0
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-01-13 12:49:35 +1100

    added a function to get the data from the api url given a search by the user and added an li containing the name of the city if the response is ok

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,8 +1,6 @@
 let city_name = "";
 let API_KEY = "3e577ad9e250c4dd28d83578156049cc";
 
-let api = `api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${API_KEY}`
-
 
 // Function to handle getting user input to the city seasrch field
 function getCity() {
@@ -13,10 +11,31 @@ function getCity() {
     cityButton.addEventListener("click", function() {
         // save city name to city name variable
         city_name = cityInput.value;
+        let api_url = `https://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${API_KEY}`
         cityInput.value = "";
+        getData(api_url);
     });
 }
 
+// Function to get the weather data
+function getData(url) {
+    // fetch the url
+    console.log(url);
+    fetch(url)
+    .then(function(response) {
+       if (response.ok) {
+        response.json()
+        .then(function(data) {
+            console.log(data);
+        });
+        let savedSeaches = document.getElementById("saved-searches-container");
+        let newSave = document.createElement("li");
+        newSave.classList.add("saved-search");
+        savedSeaches.appendChild(newSave);
+        newSave.textContent = city_name;
+       }
+    });
+}
 
 // Main function to handle execution
 function main() {
